cec635d4f8bfa42ac48e7366be97ab3d6f3350d4
Tomás Corellano <hack.rapture@gmail.com>
2014-04-14 16:32:23 -0300

    Update sx.rb

--- sx.rb
+++ sx.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Sx < Formula
   homepage 'https://github.com/spesmilo/sx'
-  url 'https://github.com/spesmilo/sx.git', :tag => 'v1.0'
+  url 'https://github.com/spesmilo/sx.git', :revision => 'a97f7bead352ac9b4e388a45128a4d7f13bdf75c'
   head 'https://github.com/spesmilo/sx.git', :branch => 'master'
 
   depends_on 'autoconf' => :build
